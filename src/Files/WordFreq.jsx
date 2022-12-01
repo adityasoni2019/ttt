e3ac4117141da250b132f2c61c257f71eeffee93
Aditya Soni <54634472+adityasoni2019@users.noreply.github.com>
2022-12-01 13:40:57 +0530

    Some changes

--- src/Files/WordFreq.jsx
+++ src/Files/WordFreq.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Histogram_visual from "../Components/Histogram_visual";
 
-
 const baseURL = "https://www.terriblytinytales.com/test.txt";
 
 const WordFreq = () => {
@@ -12,8 +11,8 @@ const WordFreq = () => {
   const [url, setUrl] = useState(null);
   const [frequencyMapSorted, setFrequencyMapSorted] = useState([]);
 
-  const[xlabel, setXlabel] = useState([]);
-  const[ylabel, setYlabel] = useState([]);
+  const [xlabel, setXlabel] = useState([]);
+  const [ylabel, setYlabel] = useState([]);
 
   useEffect(() => {
     if (url) {
@@ -36,21 +35,54 @@ const WordFreq = () => {
     // ., " ", /n, ?
 
     for (let i = 0; i < words.length; i++) {
-      if (wordMap[words[i].toLowerCase()] >=1) {
+      if (wordMap[words[i].toLowerCase()] >= 1) {
         wordMap[words[i].toLowerCase()] += 1;
       } else {
         wordMap[words[i].toLowerCase()] = 1;
         // wordMap.set(words[i].toLowerCase(),1);
       }
     }
+
     const _frequencyMapSorted = new Map([...wordMap.entries()].sort((a, b) => a[1] - b[1]));
+    // this ^ has the data in a sorted (descneding) format. And, this is a map. 
+
+    // Now, we want
+    // 1.  the data of the first 20 pairs in this map.
+    //      a. delete values after 20 
+    //      b. or store the first 20 values in a different map. -> this is more pref.
+    // 
+    // 2.  and then, divide the data in 2 arrays like this - 
+    //      a. const labels = ['2016', '2017', '2018'];
+    //      b. const data = [324, 45, 672];
+
+    // creating a new map, which would have the data of first 20. or even better, removing the data of anything more than 20.
+
     setFrequencyMapSorted(_frequencyMapSorted);
 
+    const first_20_values = new Map();
 
+    let i=0;
+    for (let [key, value] of _frequencyMapSorted) {
+      i++;
+      if(i>=20) break;
+      else first_20_values.set(key, value);
+    }
 
     console.log(frequencyMapSorted);
+    console.log("Hello, there are the 20 values");
+    console.log(first_20_values);
 
-    // sorting. wordMap  {name, freq} 
+    let labels = []; 
+    let data = []; 
+
+    let j =0;
+    for (let [key, value] of first_20_values) {
+      j++;
+      labels[j] = key;
+      data[j] = value;
+    }
+
+    // sorting. wordMap  {name, freq}
   }
 
   const handleClick = () => {
@@ -61,7 +93,6 @@ const WordFreq = () => {
     }
   };
 
-  
   return (
     <>
       <button onClick={handleClick}>SUBMIT</button>
@@ -71,7 +102,6 @@ const WordFreq = () => {
 
     </>
 
-   
   );
 };
 
